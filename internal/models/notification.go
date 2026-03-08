2d135002d153cd5b5caa72084bf1c3062c9d29ae
Gabriely <gabrielypaula98@gmail.com>
2026-03-08 10:39:23 -0300

    Create models (#2)
    * update: upgrade Go version to 1.24 and update dependencies
    
    * feat: create models for Notification, Price, and Product

---
+++ internal/models/notification.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type NotificationType string
+const (
+	NotificationPriceDrop     NotificationType = "price_drop"
+	NotificationTargetReached NotificationType = "target_reached"
+	NotificationScrapeError   NotificationType = "scrape_error"
+)
+
+type Notification struct {
+	ID        int              `json:"id"`
+	ProductID int              `json:"product_id"`
+	PriceID   *int             `json:"price_id,omitempty"`
+	Type      NotificationType `json:"type"`
+	Title     string           `json:"title"`
+	Message   string           `json:"message"`
+	Read      bool             `json:"read"`
+	CreatedAt time.Time        `json:"created_at"`
+}
